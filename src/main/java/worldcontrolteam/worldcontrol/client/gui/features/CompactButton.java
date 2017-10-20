87ad9f465762d2a0678c4cf9d039bbc96eb198ae
dmf444 <becat444@gmail.com>
2017-10-19 23:31:31 -0400

    Done for today, need to implement Server->Client quick sync

--- src/main/java/worldcontrolteam/worldcontrol/client/gui/features/CompactButton.java
+++ src/main/java/worldcontrolteam/worldcontrol/client/gui/features/CompactButton.java
@@ -3,10 +3,10 @@ package worldcontrolteam.worldcontrol.client.gui.features;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import org.lwjgl.opengl.GL11;
 
 @SideOnly(Side.CLIENT)
 public class CompactButton extends GuiButton {
@@ -20,15 +20,17 @@ public class CompactButton extends GuiButton {
     @Override
     public void drawButton(Minecraft minecraft, int par2, int par3, float doesStuff) {
         if (this.visible) {
+            GlStateManager.pushMatrix();
             FontRenderer fontRenderer = minecraft.fontRenderer;
             minecraft.renderEngine.bindTexture(TEXTURE_LOCATION);
-            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
             boolean var5 = par2 >= this.x && par3 >= this.y && par2 < this.x + this.width && par3 < this.y + this.height;
             int var6 = this.getHoverState(var5);
             this.drawTexturedModalRect(this.x, this.y, 0, 64 + var6 * 12, this.width / 2 + width % 2, this.height);
             this.drawTexturedModalRect(this.x + this.width / 2 + width % 2, this.y, 200 - this.width / 2, 64 + var6 * 12, this.width / 2, this.height);
             this.mouseDragged(minecraft, par2, par3);
             fontRenderer.drawString(displayString, x + (width - fontRenderer.getStringWidth(displayString)) / 2, y + 2, 0x303030);
+            GlStateManager.popMatrix();
         }
     }
 
