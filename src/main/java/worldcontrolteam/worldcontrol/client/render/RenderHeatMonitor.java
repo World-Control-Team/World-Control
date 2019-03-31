b2d7981c756dfc6f3d2b0da859e35a50f698f17c
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2019-03-31 16:24:43 -0400

    Fix render code on heatmap
    attn @dmf444, needs texturing and position fixes

--- src/main/java/worldcontrolteam/worldcontrol/client/render/RenderHeatMonitor.java
+++ src/main/java/worldcontrolteam/worldcontrol/client/render/RenderHeatMonitor.java
@@ -6,6 +6,7 @@ import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.init.Blocks;
@@ -15,6 +16,7 @@ import net.minecraft.util.math.MathHelper;
 import org.lwjgl.opengl.GL11;
 import worldcontrolteam.worldcontrol.blocks.BlockBasicRotate;
 import worldcontrolteam.worldcontrol.tileentity.TileEntityBaseReactorHeatMonitor;
+import worldcontrolteam.worldcontrol.utils.WCUtility;
 
 public class RenderHeatMonitor extends TileEntitySpecialRenderer<TileEntityBaseReactorHeatMonitor> {
     private static final String TEXTURE_FILE = "worldcontrol:textures/blocks/remoteheatmonitor/scale.png";
@@ -35,8 +37,8 @@ public class RenderHeatMonitor extends TileEntitySpecialRenderer<TileEntityBaseR
         if(te.getRenderType().equals("half_block")) {
             renderNumberOnBlock(te.getThreshhold(), getWorld().getBlockState(te.getPos()).getValue(BlockBasicRotate.FACING), 0, x, y, z);
         } else if(te.getRenderType().equals("full_block")){
-            renderNumberOnBlock(te.getThreshhold(), getWorld().getBlockState(te.getPos()).getValue(BlockBasicRotate.FACING), 0.58f, x, y, z);
             drawHeatMap(te, getWorld().getBlockState(te.getPos()).getValue(BlockBasicRotate.FACING), x, y, z);
+            renderNumberOnBlock(te.getThreshhold(), getWorld().getBlockState(te.getPos()).getValue(BlockBasicRotate.FACING), 0.58f, x, y, z);
         }
 
         GlStateManager.popAttrib();
@@ -87,7 +89,7 @@ public class RenderHeatMonitor extends TileEntitySpecialRenderer<TileEntityBaseR
         GlStateManager.scale(var12, -var12, var12);
         GlStateManager.doPolygonOffset(-10, -10);
         GlStateManager.enablePolygonOffset();
-        getFontRenderer().drawString(String.valueOf(threshold), -getFontRenderer().getStringWidth(String.valueOf(threshold)) / 2, -getFontRenderer().FONT_HEIGHT, 0);
+        getFontRenderer().drawString(String.valueOf(threshold), -getFontRenderer().getStringWidth(String.valueOf(threshold)) / 2, -getFontRenderer().FONT_HEIGHT, 0xFF00FF);
         GlStateManager.disablePolygonOffset();
         GlStateManager.color(1F, 1F, 1F, 1F);
         GlStateManager.popMatrix();
@@ -98,24 +100,55 @@ public class RenderHeatMonitor extends TileEntitySpecialRenderer<TileEntityBaseR
         // Setup transforms for block face -- copied straight out of the infopanel.
 
         GlStateManager.pushMatrix();
-        RenderInfoPanel.setupFaceTransforms(x, y, z, facing); // Sets up model matrix to map 0-64 x 0-64 y to the block face.
 
         // Bind textures
         bindTexture(TEXTURE_LOCATION);
+
+        short side = (short) facing.getIndex();
+        GlStateManager.translate((float) x, (float) y, (float) z);
+        switch (side) {
+            case 0:
+                break;
+            case 1:
+                GlStateManager.translate(1, 1, 0);
+                GlStateManager.rotate(180, 1, 0, 0);
+                GlStateManager.rotate(180, 0, 1, 0);
+                break;
+            case 2:
+                GlStateManager.translate(0, 1, 0);
+                GlStateManager.rotate(0, 0, 1, 0);
+                GlStateManager.rotate(90, 1, 0, 0);
+                break;
+            case 3:
+                GlStateManager.translate(1, 1, 1);
+                GlStateManager.rotate(180, 0, 1, 0);
+                GlStateManager.rotate(90, 1, 0, 0);
+                break;
+            case 4:
+                GlStateManager.translate(0, 1, 1);
+                GlStateManager.rotate(90, 0, 1, 0);
+                GlStateManager.rotate(90, 1, 0, 0);
+                break;
+            case 5:
+                GlStateManager.translate(1, 1, 0);
+                GlStateManager.rotate(-90, 0, 1, 0);
+                GlStateManager.rotate(90, 1, 0, 0);
+                break;
+        }
+        GlStateManager.rotate(-90, 1, 0, 0);
+        GlStateManager.translate(0, -1, 0);
 
         Tessellator t = Tessellator.getInstance();
         BufferBuilder b = t.getBuffer();
 
         // Show thing
-        int amt = (int)(64 * MathHelper.clamp(baseReactorHeatMonitor.getThreshhold() / (double)baseReactorHeatMonitor.getCurrentHeat(), 0.0D, 1.0D));
-        double other_amt = (double)amt / 64;
+        double amt = MathHelper.clamp(baseReactorHeatMonitor.getThreshhold() / (double)baseReactorHeatMonitor.getCurrentHeat(), 0.5D, 1.0D);
 
-        b.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR);
-        b.pos(0, 0, 0).tex(0, 0).color(1, 1, 1, 1).endVertex();
-        b.pos(amt, 0, 0).tex(other_amt, 0).color(1, 1, 1, 1).endVertex();
-        b.pos(amt, 64, 0).tex(other_amt, 1).color(1, 1, 1, 1).endVertex();
-        b.pos(0, 64, 0).tex(0, 1).color(1, 1, 1, 1).endVertex();
-
+        b.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
+        b.pos(1 - amt, 0.15625, -0.01).tex(amt, 0).endVertex();
+        b.pos(1 - amt, 0.5, -0.01).tex(amt, 0.8).endVertex();
+        b.pos(1, 0.5, -0.01).tex(0, 0.8).endVertex();
+        b.pos(1, 0.15625, -0.01).tex(0, 0).endVertex();
         t.draw();
 
         GlStateManager.popMatrix();
